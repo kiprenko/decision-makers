5ca5b6ca8b4eda9a1d64a2e1fab7b0d35cd98281
yunusc5n <ycsakalli@gmail.com>
2021-12-05 16:50:49 +0300

    translate to turkish and misc.

--- decision-maker-1.py
+++ decision-maker-1.py
@@ -1,56 +1,52 @@
 import csv
 import sys
-
+import numpy as np
 
 def run_app(data_file_name):
-    available_criterions = {1: wald_criterion,
-                  2: optimism_criterion,
-                  3: pessimism_criterion,
-                  4: hurwitz_criterion,
-                  5: savage_criterion}
+    available_criterions = {
+        1: iyimserlik_kriteri,
+        2: kötümserlik_kriteri,
+        3: hurwitz_kriteri,
+        4: savage_kriteri
+    }
     with open(data_file_name, newline='') as csv_file:
         data = {entry[0]: entry[1:] for entry in list(csv.reader(csv_file))}
-    menu_option = input('Available options:\n'
-                        '    1. Wald criterion\n'
-                        '    2. optimism criterion\n'
-                        '    3. pessimism criterion\n'
-                        '    4. Hurwitz criterion\n'
-                        '    5. Savage criterion\n'
-                        'Choose the number of option: ')
+
+
+    menu_option = input('Secenekler:\n'
+                        '    1. İyimserlik kriteri\n'
+                        '    2. Kötümserlik kriteri\n'
+                        '    3. Hurwitz kriteri\n'
+                        '    4. Savage kriteri\n'
+                        'Uygun olan seçenek: ')
     try:
         available_criterions[int(menu_option)](data)
     except KeyError and ValueError:
-        print('Invalid menu value provided. Exiting.')
+        print('Gecersiz secenek degeri girildi. Cikiliyor...')
 
 
-def wald_criterion(data):
-    min_val = get_mins(data)
-    maximin = max(min_val.values())
-    answer = [k for k, v in min_val.items() if v == maximin]
-    print(f'According to Wald\'s criterion, the optimal strategy is {answer}')
 
-
-def optimism_criterion(data):
+def iyimserlik_kriteri(data):
     max_val = get_maxs(data)
     maximax = max(max_val.values())
     answer = [k for k, v in max_val.items() if v == maximax]
-    print(f'According to optimism criterion, the optimal strategy is {answer}')
+    print(f'İyimserlik kriterine göre, optimal strateji {answer}')
 
 
-def pessimism_criterion(data):
+def kötümserlik_kriteri(data):
     min_val = get_mins(data)
-    minimin = min(min_val.values())
-    answer = [k for k, v in min_val.items() if v == minimin]
-    print(f'According to pessimism criterion, the optimal strategy is {answer}')
+    minimax = max(min_val.values())
+    answer = [k for k, v in min_val.items() if v == minimax]
+    print(f'Kötümserlik kriterine göre, optimal strateji {answer}')
 
 
-def hurwitz_criterion(data):
+def hurwitz_kriteri(data):
     try:
-        optimism_value = float(input('Write your λ, 0 ≤ λ ≤ 1: '))
+        optimism_value = float(input('λ Degeri girin, 0 ≤ λ ≤ 1: '))
         if optimism_value > 1 or optimism_value < 0:
             raise ValueError
     except ValueError:
-        print('Incorrect λ specified. Exiting')
+        print('Yanlış λ belirlendi. Cikiliyor')
         sys.exit()
 
     max_vals = get_maxs(data)
@@ -61,10 +57,10 @@ def hurwitz_criterion(data):
         hurwitz_values[s_name] = int(max_val) * optimism_value + n * int(min_vals[s_name])
     max_hurwitz_value = max(hurwitz_values.values())
     answer = [k for k, v in hurwitz_values.items() if v == max_hurwitz_value]
-    print(f'According to Hurwitz criterion, the optimal strategy is {answer}')
+    print(f'Hurwitz kriterine göre, optimal strateji {answer}')
 
 
-def savage_criterion(data):
+def savage_kriteri(data):
     max_profits = []
     for i in range(len(next(iter(data.values())))):
         max_profits.append(max([value[i] for value in data.values()]))
@@ -76,7 +72,7 @@ def savage_criterion(data):
         max_regrets[s_name] = max(regrets)
     minimal_max_regret = min(max_regrets.values())
     answer = [k for k, v in max_regrets.items() if v == minimal_max_regret]
-    print(f'According to Savage criterion, the optimal strategy is {answer}')
+    print(f'Savage kriterine göre, optimal strateji {answer}')
 
 
 def get_mins(data):
@@ -89,6 +85,6 @@ def get_maxs(data):
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print('You have not specified the file name!')
+        print('Dosya adını girmelisiniz!')
         sys.exit()
     run_app(sys.argv[1])
