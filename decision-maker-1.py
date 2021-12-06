e0b9e62a2f587ceeb188849b4d551a395476285e
yunusc5n <ycsakalli@gmail.com>
2021-12-06 21:31:39 +0300

    biraz calisiyor

--- decision-maker-1.py
+++ decision-maker-1.py
@@ -2,15 +2,41 @@ import csv
 import sys
 import numpy as np
 
+PROBABILITY_KEY = 'olasilik'
+def get_strategies(data):
+    return {key: value for key, value in data.items() if key != PROBABILITY_KEY}
+
 def run_app(data_file_name):
+    esas_criterions = {
+        1: gelir_esasli,
+        2: maliyet_esasli,
+    }
+    with open(data_file_name, newline='') as csv_file:
+        data = {entry[0]: entry[1:] for entry in list(csv.reader(csv_file))}
+
+
+    esas_option = input('Secenekler:\n'
+                        '    1. Gelir esaslı\n'
+                        '    2. Maliyet esaslı\n'
+                        'Uygun olan seçenek: '
+                        )
+    try:
+        esas_criterions[int(esas_option)](data)
+    except KeyError and ValueError:
+        print('Gecersiz secenek degeri girildi. Cikiliyor...')
+
+
+
+
+def gelir_esasli(data):
+
     available_criterions = {
         1: iyimserlik_kriteri,
         2: kötümserlik_kriteri,
         3: hurwitz_kriteri,
-        4: savage_kriteri
+        4: savage_kriteri,
+        5: laplace_kriteri
     }
-    with open(data_file_name, newline='') as csv_file:
-        data = {entry[0]: entry[1:] for entry in list(csv.reader(csv_file))}
 
 
     menu_option = input('Secenekler:\n'
@@ -18,12 +44,32 @@ def run_app(data_file_name):
                         '    2. Kötümserlik kriteri\n'
                         '    3. Hurwitz kriteri\n'
                         '    4. Savage kriteri\n'
+                        '    5. Laplace kriteri\n'
                         'Uygun olan seçenek: ')
     try:
         available_criterions[int(menu_option)](data)
     except KeyError and ValueError:
         print('Gecersiz secenek degeri girildi. Cikiliyor...')
 
+def maliyet_esasli(data):
+
+    available_criterions = {
+        1: iyimserlik_kriteri_maliyet,
+        2: kötümserlik_kriteri_maliyet,
+        #3: hurwitz_kriteri_maliyet,
+        #4: savage_kriteri_maliyet,
+        5: laplace_kriteri_maliyet
+    }
+
+    menu_option = input('Secenekler:\n'
+                        '    1. İyimserlik kriteri\n'
+                        '    2. Kötümserlik kriteri\n'
+                        '    5. Laplace kriteri\n'
+                        'Uygun olan seçenek: ')
+    try:
+        available_criterions[int(menu_option)](data)
+    except KeyError and ValueError:
+        print('Gecersiz secenek degeri girildi. Cikiliyor...')
 
 
 def iyimserlik_kriteri(data):
@@ -32,6 +78,11 @@ def iyimserlik_kriteri(data):
     answer = [k for k, v in max_val.items() if v == maximax]
     print(f'İyimserlik kriterine göre, optimal strateji {answer}')
 
+def iyimserlik_kriteri_maliyet(data):
+    min_val = get_mins(data)
+    minimin = min(min_val.values())
+    answer = [k for k, v in min_val.items() if v == minimin]
+    print(f'İyimserlik kriterine göre, optimal strateji {answer}')
 
 def kötümserlik_kriteri(data):
     min_val = get_mins(data)
@@ -39,6 +90,11 @@ def kötümserlik_kriteri(data):
     answer = [k for k, v in min_val.items() if v == minimax]
     print(f'Kötümserlik kriterine göre, optimal strateji {answer}')
 
+def kötümserlik_kriteri_maliyet(data):
+    max_val = get_maxs(data)
+    maximin = min(max_val.values())
+    answer = [k for k, v in max_val.items() if v == maximin]
+    print(f'Kötümserlik kriterine göre, optimal strateji {answer}')
 
 def hurwitz_kriteri(data):
     try:
@@ -73,6 +129,38 @@ def savage_kriteri(data):
     minimal_max_regret = min(max_regrets.values())
     answer = [k for k, v in max_regrets.items() if v == minimal_max_regret]
     print(f'Savage kriterine göre, optimal strateji {answer}')
+
+def laplace_kriteri(strategies):
+    count_of_env_states = len(next(iter(strategies.values())))
+    strat_weights = {}
+    for strat_name, values in strategies.items():
+        strat_weights[strat_name] = sum([int(value) for value in values]) / count_of_env_states
+    print_results(strat_weights, 'Laplace')
+
+def laplace_kriteri_maliyet(strategies, probabilities):
+    count_of_env_states = len(next(iter(strategies.values())))
+    strat_weights = {}
+    for strat_name, values in strategies.items():
+        strat_weights[strat_name] = sum([int(value) for value in values]) / count_of_env_states
+    print_results_maliyet(strat_weights, 'Laplace')
+
+def prettify(iterable):
+    import json
+    return json.dumps(iterable, indent=4)
+
+def print_results(strat_weights, strat_name):
+    print('Strategies weights:\n', prettify(strat_weights))
+    max_weight = max(strat_weights.values())
+    answer = [k for k, v in strat_weights.items() if v == max_weight]
+    print(f'The biggest weight {max_weight} has strategy(-ies) {answer}')
+    print(f'According to {strat_name} criterion, the optimal strategy is {answer}')
+
+def print_results_maliyet(strat_weights, strat_name):
+    print('Strategies weights:\n', prettify(strat_weights))
+    min_weight = min(strat_weights.values())
+    answer = [k for k, v in strat_weights.items() if v == min_weight]
+    print(f'The biggest weight {min_weight} has strategy(-ies) {answer}')
+    print(f'According to {strat_name} criterion, the optimal strategy is {answer}')
 
 
 def get_mins(data):
